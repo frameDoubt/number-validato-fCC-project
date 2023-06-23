99c54c8677ff3c58aa9d69a2fdfef2f36549d08e
Brandon Wright <brandon.w.wright4@gmail.com>
2023-06-23 16:42:57 -0700

    add regular expressions

--- telephoneCheck.js
+++ telephoneCheck.js
@@ -1,5 +1,11 @@
 function telephoneCheck(str) {
+  // only matches once in a properly formatted "number"
+  const regEx1 = /\(\d\d\d\)/g;
+  // can match twice in a properly formatted "number"
+  const regEx2 = /\d\d\d-/;
+  const regEx3 = /\d{10}/;
+  console.log(regEx3.exec(str));
   return true;
 }
 
-telephoneCheck("555-555-5555");
+telephoneCheck("5555555555");
