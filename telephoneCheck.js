45d17705edf6e897c1c46e96192d6e8442fab750
Brandon Wright <brandon.w.wright4@gmail.com>
2023-06-23 21:36:37 -0700

    add jank code

--- telephoneCheck.js
+++ telephoneCheck.js
@@ -1,11 +1,30 @@
 function telephoneCheck(str) {
-  // only matches once in a properly formatted "number"
-  const regEx1 = /\(\d\d\d\)/g;
-  // can match twice in a properly formatted "number"
-  const regEx2 = /\d\d\d-/;
-  const regEx3 = /\d{10}/;
-  console.log(regEx3.exec(str));
+  const formattedStr = str.replace(/\s/g, '');
+// only matches once in a properly formatted "number"
+const regEx1 = /\(\d\d\d\)/;
+// can match twice in a properly formatted "number"
+const regEx2 = /\d\d\d-/;
+// can match once in a properly formatted "number"
+// tests for 10 digits repeated
+const regEx3 = /\d{10}/;
+// checks for 4 repeating digits
+const regEx4 = /\d{4}/;
+if (regEx3.test(formattedStr) || /1\d{10}/.formattedStr) {
+  console.log(formattedStr);
   return true;
+};
+if (formattedStr[0] === '1' && (formattedStr.length >= 11 && formattedStr.length <= 14)) {
+  if (regEx1.test(formattedStr) && regEx2.test(formattedStr)) {
+    return true;
+  }
+  if (/(\d{3}-){2}/.test(formattedStr) && formattedStr.length <= 13) {
+    return true;
+  }
+}
+if (regEx1.test(formattedStr) && regEx2.test(formattedStr)) {
+
+}
+return false;
 }
 
-telephoneCheck("5555555555");
+console.log(telephoneCheck("1(555)555-5555"));
